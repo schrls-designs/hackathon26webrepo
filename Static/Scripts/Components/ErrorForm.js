c37bd030b0d4e68625dc013cdab0edbf0fcf565a
SIndustryPortfolio <s.industry@outlook.com>
2026-03-12 14:29:34 +0000

    .

--- Static/Scripts/Components/ErrorForm.js
+++ Static/Scripts/Components/ErrorForm.js
@@ -4,6 +4,7 @@ import UtilitiesService from "../Services/Utilities.js";
 
 
 // CORE
+const ErrorAudio = new Audio("/Static/Audio/Error.mp3");
 
 // CLASS
 class ErrorFormComponent
@@ -16,8 +17,17 @@ class ErrorFormComponent
     Initialise() 
     {
         // Functions
-        // 
-        
+        //
+
+        const x = ((Math.random() * window.innerWidth) / 2) - (window.innerWidth / 4);
+        const y = ((Math.random() * window.innerHeight) / 2) - (window.innerHeight / 4);
+
+        this.Element.style.position = "fixed";
+        this.Element.style.zIndex = "10";
+        this.Element.style.top = y + "px";
+        this.Element.style.left = x + "px";
+
+        ErrorAudio.play();
     }
 
     End() 
